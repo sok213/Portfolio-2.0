1bd0f06ab05f10056a8d15323ce111dc90b0a207
Sophanarith Sok <sok213@Alien-Macbook-Pro.local>
2018-12-06 23:08:57 -0800

    Create panel component

---
+++ src/components/NavigationDots/NavigationDots.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import s from './NavigationDots.scss';
+
+class NavigationDots extends Component {
+	render() {
+		return (
+			<div>
+                <p>Navigation Dots Component</p>
+			</div>
+		)
+	}
+}
+
+export default NavigationDots;
---
+++ src/components/Panels/Panels.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import s from './Panels.scss';
+
+class Panels extends Component {
+    render() {
+        return (
+            <div className={s.panelsContainer}>
+                <p>Panels Container</p>
+            </div>
+        )
+    }
+}
+
+export default Panels;
