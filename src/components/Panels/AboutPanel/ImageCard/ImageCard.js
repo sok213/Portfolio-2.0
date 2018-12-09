aafdcbf17563adc9caa82fbc8e969ddcf1219de4
Sophanarith Sok <sok213@Alien-Macbook-Pro.local>
2018-12-09 11:37:41 -0800

    Change animation classes. Update imageCard Component

--- src/components/Panels/AboutPanel/ImageCard/ImageCard.js
+++ src/components/Panels/AboutPanel/ImageCard/ImageCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import s from './ImageCard.module.scss';
 import PropTypes from 'prop-types';
+import { Animated } from 'react-animated-css';
 
 class ImageCard extends Component {
     constructor(props) {
@@ -14,7 +15,11 @@ class ImageCard extends Component {
     render() {
         return (
             <div className={s.imageCardContainer}>
-                <p>ImageCard Component.</p>
+                <Animated animationIn="fadeInDown" isVisible={true} className={s.test}>
+                    <div className={s.imageCardContent}>
+                        <p>ImageCard Component.</p>
+                    </div>
+                </Animated>
             </div>
         )
     }
--- src/components/Panels/AboutPanel/TextCard/TextCard.js
+++ src/components/Panels/AboutPanel/TextCard/TextCard.js
@@ -15,7 +15,7 @@ class TextCard extends Component {
     render() {
         return (
             <div className={s.textCardContainer}>
-                <Animated animationIn="bounceInLeft" isVisible={true}>
+                <Animated animationIn="slideInLeft" isVisible={true}>
                     <div className={s.textContent}>
                         <h1>Hello.</h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
