34cb5a8478e3de62dfdf2dac7e62c6858b5ab80f
Sophanarith Sok <sok213@Alien-Macbook-Pro.local>
2018-12-11 08:26:19 -0800

    Add hrefs to links

--- src/components/Panels/DiscoverPanel/LinksPanel/LinksPanel.js
+++ src/components/Panels/DiscoverPanel/LinksPanel/LinksPanel.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 import s from './LinksPanel.module.scss';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faYoutube, faFreeCodeCamp, faCodepen, faLinkedin, faMedium } from '@fortawesome/free-brands-svg-icons';
+import { faMailBulk } from '@fortawesome/free-solid-svg-icons';
+import { 
+    faYoutube, 
+    faGithub, 
+    faCodepen, 
+    faLinkedin, 
+    faMedium 
+} from '@fortawesome/free-brands-svg-icons';
 
 class LinksPanel extends Component {
     constructor(props) {
@@ -17,23 +24,38 @@ class LinksPanel extends Component {
         return (
             <div className={s.linksPanelContainer}>
                 <div className={s.linksPanel}>
-                    <h4>Social Media</h4>
+                    <h3>Connect</h3>
                     <div className={s.underline}></div>
                     <ul className={s.socialMediaLinks}>
                         <li>
-                            <a href="#"><FontAwesomeIcon icon={faLinkedin} /></a>
+                            <a href="https://www.linkedin.com/in/sophanarith-sok-a50097b7/" target="_blank">
+                                <FontAwesomeIcon icon={faLinkedin} />
+                            </a>
                         </li>
                         <li>
-                            <a href="#"><FontAwesomeIcon icon={faMedium} /></a>
+                            <a href="https://medium.com/@sok213" target="_blank">
+                                <FontAwesomeIcon icon={faMedium} />
+                            </a>
                         </li>
                         <li>
-                            <a href="#"><FontAwesomeIcon icon={faCodepen} /></a>
+                            <a href="mailto:sokcodes@gmail.com">
+                                <FontAwesomeIcon icon={faMailBulk} />
+                            </a>
                         </li>
                         <li>
-                            <a href="#"><FontAwesomeIcon icon={faFreeCodeCamp} /></a>
+                            <a href="https://codepen.io/sok213/" target="_blank">
+                                <FontAwesomeIcon icon={faCodepen} />
+                            </a>
                         </li>
                         <li>
-                            <a href="#"><FontAwesomeIcon icon={faYoutube} /></a>
+                            <a href="https://github.com/sok213" target="_blank">
+                                <FontAwesomeIcon icon={faGithub} />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.youtube.com/watch?v=lX64QPxtjBs" target="_blank">
+                                <FontAwesomeIcon icon={faYoutube} />
+                            </a>
                         </li>
                     </ul>
                 </div>
