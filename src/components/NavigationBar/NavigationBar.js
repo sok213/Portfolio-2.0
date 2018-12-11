414f938999bc41f41724bf97992354a643e6f6d1
Sophanarith Sok <sok213@Alien-Macbook-Pro.local>
2018-12-11 08:59:03 -0800

    Integrate active panel color

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -4,28 +4,49 @@ import PropTypes from 'prop-types';
 
 class NavigationBar extends Component {
 
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			activePanel: ''
+		}
+	}
+
 	static propTypes = {
 		navigateTo: PropTypes.func
 	}
 
-	navigateTo = (page) => {
+	navigateTo = (page, tabClicked) => {
 		this.props.navigateTo(page);
+		this.setState({ activePanel: tabClicked });
 	}
 
 	render() {
 		return (
 			<div className={s.navigationContainer}>
 				<ul className={s.navItemsContainer}>
-					<li onClick={this.navigateTo.bind(this, 1)}>
+					<li 
+						onClick={this.navigateTo.bind(this, 1, 'hi')}
+						className={this.state.activePanel == 'hi' ? s.active : null}
+					>
 						<span>Hi</span>
 					</li>
-					<li onClick={this.navigateTo.bind(this, 2)}>
+					<li 
+						onClick={this.navigateTo.bind(this, 2, 'bio')}
+						className={this.state.activePanel == 'bio' ? s.active : null}	
+					>
 						<span>Bio</span>
 					</li>
-					<li onClick={this.navigateTo.bind(this, 3)}>
+					<li 
+						onClick={this.navigateTo.bind(this, 3, 'dev')}
+						className={this.state.activePanel == 'dev' ? s.active : null}	
+					>
 						<span>Dev</span>
 					</li>
-					<li onClick={this.navigateTo.bind(this, 4)}>
+					<li 
+						onClick={this.navigateTo.bind(this, 4, 'discover')}
+						className={this.state.activePanel == 'discover' ? s.active : null}	
+					>
 						<span>Discover</span>
 					</li>
 				</ul>
