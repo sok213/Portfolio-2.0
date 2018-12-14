0369c05fde6f808398a5de6abd1e2e326a29da27
Sophanarith Sok <sok213@Alien-Macbook-Pro.local>
2018-12-13 17:46:01 -0800

    Integrate feedmirror api.

--- src/components/Panels/DiscoverPanel/BlogPanel/BlogPanel.js
+++ src/components/Panels/DiscoverPanel/BlogPanel/BlogPanel.js
@@ -5,16 +5,21 @@ import PropTypes from 'prop-types';
 class BlogPanel extends Component {
     constructor(props) {
         super(props); 
-        
-        this.state = {
 
+        this.state = {
+            
         }
+    }
+
+    componentDidMount() {
+        window.feedMirror();
     }
 
     render() {
         return (
             <div className={s.blogPanelContainer}>
-                <p>BlogPanel Component.</p>
+                <p>Medium posts should render here.</p>
+                <div id="fm-medium-embed"></div>
             </div>
         )
     }
