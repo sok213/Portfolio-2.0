2bf189935ad7ef641d28701dda95b84550c0a5ae
Sophanarith Sok <sok213@Alien-Macbook-Pro.local>
2018-12-18 14:16:57 -0800

    Remove unused asset and scale down and compress cafe.jpeg

--- src/components/Panels/WelcomePanel/WelcomePanel.js
+++ src/components/Panels/WelcomePanel/WelcomePanel.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import s from './WelcomePanel.module.scss';
 import PropTypes from 'prop-types';
-import wallofbooks from '../../../assets/wallofbooks_vibrant.jpeg';
 
 class WelcomePanel extends Component {
     constructor(props) {
