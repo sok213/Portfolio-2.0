d895d3030eafce82cc43b516ef4d409259713448
Sophanarith Sok <sok213@Alien-Macbook-Pro.local>
2019-01-01 22:18:12 -0800

    Change name of item 1

--- src/components/Panels/DevPanel/DevPanel.js
+++ src/components/Panels/DevPanel/DevPanel.js
@@ -10,7 +10,7 @@ class DevPanel extends Component {
         this.state = {
             devItems: [
                 {
-                    title: 'Questhetics',
+                    title: 'Questhetical',
                     desc: <p>A website built with React to assist people to succeed in their quest for achieving an <em>aesthetic</em> physique based on their body type.</p>,
                     animation: 'fadeInLeft',
                     thumbnail: 1,
